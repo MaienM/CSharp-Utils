0749264e9f82024e3c35da021e381e61f2862015
Michon van Dooren <michon1992@gmail.com>
2016-02-16 23:34:46 +0100

    Created a service manager to perform actions on services from the command line

--- CSharp-Utils/Utils/ServicesCollection.cs
+++ CSharp-Utils/Utils/ServicesCollection.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Configuration.Install;
 using System.Reflection;
 using System.ServiceProcess;
 
@@ -7,6 +9,9 @@ namespace CSharpUtils.Utils
 {
     public class ServicesCollection : List<ServiceBase>
     {
+        /// <summary>
+        /// Automatically either run or simulate the services.
+        /// </summary>
         public void Auto()
         {
             if (Environment.UserInteractive)
@@ -19,7 +24,10 @@ namespace CSharpUtils.Utils
             }
         }
 
-        public void SimulateStart()
+        /// <summary>
+        /// Simulate starting the services.
+        /// </summary>
+        private void SimulateStart()
         {
             // Call OnStart method.
             foreach (ServiceBase service in this)
@@ -29,7 +37,10 @@ namespace CSharpUtils.Utils
             }
         }
 
-        public void SimulateStop()
+        /// <summary>
+        /// Simulate stopping the services.
+        /// </summary>
+        private void SimulateStop()
         { 
             // Call OnStop method.
             foreach (ServiceBase service in this)
@@ -39,6 +50,11 @@ namespace CSharpUtils.Utils
             }
         }
 
+        /// <summary>
+        /// Simulate running the services.
+        /// 
+        /// This invokes the OnStart method of all services, waits for input from the user, and then invokes the OnStop method of all services.
+        /// </summary>
         public void Simulate()
         {
             SimulateStart();
@@ -47,11 +63,17 @@ namespace CSharpUtils.Utils
             SimulateStop();
         }
 
+        /// <summary>
+        /// Run the services.
+        /// </summary>
         public void Run()
         {
             ServiceBase.Run(this.ToArray());
         }
 
+        /// <summary>
+        /// Invoke the start task on the _installed_ services.
+        /// </summary>
         public void InvokeStart()
         {
             foreach (ServiceBase service in this)
@@ -69,6 +91,9 @@ namespace CSharpUtils.Utils
             }
         }
 
+        /// <summary>
+        /// Invoke the stop task on the _installed_ services.
+        /// </summary>
         public void InvokeStop()
         {
             foreach (ServiceBase service in this)
@@ -86,6 +111,9 @@ namespace CSharpUtils.Utils
             }
         }
 
+        /// <summary>
+        /// Invoke the restart task on the _installed_ services.
+        /// </summary>
         public void InvokeRestart()
         {
             foreach (ServiceBase service in this)
@@ -103,6 +131,68 @@ namespace CSharpUtils.Utils
                 {
                     Console.Error.WriteLine("Failed to restart service {0}", service.ServiceName);
                 }
+            }
+        }
+
+        /// <summary>
+        /// Install the services.
+        /// </summary>
+        public void Install()
+        {
+            DoInstall(false);
+        }
+
+        /// <summary>
+        /// Uninstall the services.
+        /// </summary>
+        public void Uninstall()
+        {
+            DoInstall(true);
+        }
+
+        /// <summary>
+        /// Reinstall the services.
+        /// </summary>
+        public void Reinstall()
+        {
+            Uninstall();
+            Install();
+        }
+
+        private void DoInstall(bool undo)
+        {
+            try
+            {
+                using (AssemblyInstaller inst = new AssemblyInstaller(this[0].GetType().Assembly, new string[0]))
+                {
+                    IDictionary state = new Hashtable();
+                    inst.UseNewContext = true;
+                    try
+                    {
+                        if (undo)
+                        {
+                            inst.Uninstall(state);
+                        }
+                        else
+                        {
+                            inst.Install(state);
+                            inst.Commit(state);
+                        }
+                    }
+                    catch
+                    {
+                        try
+                        {
+                            inst.Rollback(state);
+                        }
+                        catch { }
+                        throw;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
             }
         }
     }
---
+++ CSharp-Utils/Utils/ServicesManager.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CSharpUtils.Utils
+{
+    public class ServicesManager
+    {
+        private struct ActionData
+        {
+            public Action<ServicesCollection> Action { get; set; }
+            public string Description { get; set; }
+        }
+
+        /// <summary>
+        /// The managed services.
+        /// </summary>
+        private ServicesCollection services;
+
+        /// <summary>
+        /// The actions that can be invoked.
+        /// </summary>
+        private Dictionary<string, ActionData> actions = new Dictionary<string, ActionData>();
+
+        /// <summary>
+        /// Called before an action is performed.
+        /// </summary>
+        public EventHandler<ActionEventArgs> BeforeAction = (object sender, ActionEventArgs e) => { };
+
+        /// <summary>
+        /// Called after an action has been performed.
+        /// </summary>
+        public EventHandler<ActionEventArgs> AfterAction = (object sender, ActionEventArgs e) => { };
+
+        public ServicesManager(ServicesCollection services)
+        {
+            this.services = services;
+
+            RegisterAction("install", "Install the service", ActionInstall);
+            RegisterAction("uninstall", "Uninstall the service", ActionUninstall);
+            RegisterAction("reinstall", "Reinstall the service", ActionReinstall);
+            RegisterAction("start", "Start the service", ActionStart);
+            RegisterAction("stop", "Stop the service", ActionStop);
+            RegisterAction("restart", "Restart the service", ActionRestart);
+            RegisterAction("simulate", null, ActionSimulate);
+        }
+
+        /// <summary>
+        /// Register an custom action.
+        /// </summary>
+        /// <param name="name">The name of the action</param>
+        /// <param name="description">The description of the action</param>
+        /// <param name="action">The action to call</param>
+        public void RegisterAction(string name, string description, Action<ServicesCollection> action)
+        {
+            if (actions.ContainsKey(name))
+            {
+                throw new ArgumentException(string.Format("Action name {0} is already taken", name), "name");
+            }
+
+            actions.Add(name, new ActionData { Action = action, Description = description });
+        }
+
+        /// <summary>
+        /// Process the command line arguments, and perform the requested actions.
+        /// </summary>
+        /// <param name="args"></param>
+        public void Run(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                if (Environment.UserInteractive)
+                {
+                    // No arguments + interactive = show usage
+                    Usage();
+                }
+                else
+                {
+                    // No arguments + non-interactive = the service is being started.
+                    services.Run();
+                }
+            }
+            else
+            {
+                foreach (string arg in args)
+                {
+                    ActionData action;
+                    if (actions.TryGetValue(arg, out action))
+                    {
+                        // Action exists, call it.
+                        BeforeAction(this, new ActionEventArgs(arg));
+                        action.Action.Invoke(services);
+                        AfterAction(this, new ActionEventArgs(arg));
+                    }
+                    else
+                    {
+                        // Action does not exits, call the method and stop/continue depending on return value.
+                        if (!ActionNotFound(arg))
+                        {
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void ActionInstall(ServicesCollection services)
+        {
+            Console.WriteLine("Installing service...");
+            services.Install();
+        }
+
+        private void ActionUninstall(ServicesCollection services)
+        {
+            Console.WriteLine("Uninstalling service...");
+            services.InvokeStop();
+            services.Uninstall();
+        }
+
+        private void ActionReinstall(ServicesCollection services)
+        {
+            Console.WriteLine("Reinstalling service...");
+            services.InvokeStop();
+            services.Reinstall();
+        }
+
+        private void ActionSimulate(ServicesCollection services)
+        {
+            Console.WriteLine("Simulating service...");
+            services.Simulate();
+        }
+
+        private void ActionStart(ServicesCollection services)
+        {
+            Console.WriteLine("Starting service...");
+            services.InvokeStart();
+        }
+
+        private void ActionStop(ServicesCollection services)
+        {
+            Console.WriteLine("Stoping service...");
+            services.InvokeStop();
+        }
+
+        private void ActionRestart(ServicesCollection services)
+        {
+            Console.WriteLine("Restarting service...");
+            services.InvokeRestart();
+        }
+
+        /// <summary>
+        /// Called when an unknown action is passed as an argument.
+        /// </summary>
+        /// <param name="name">The name of the action</param>
+        /// <returns>Whether to continue running</returns>
+        protected bool ActionNotFound(string name)
+        {
+            Usage();
+            return false;
+        }
+
+        /// <summary>
+        /// Output the usage of the script.
+        /// </summary>
+        protected void Usage()
+        {
+            string exeName = Path.GetFileName(System.Reflection.Assembly.GetEntryAssembly().CodeBase);
+            string serviceText = services.Count > 1 ? "services" : "service";
+
+            Console.Error.WriteLine("Usage: {0} ACTION ...", exeName);
+            Console.Error.WriteLine("Actions: (all actions require admin access)");
+            foreach (KeyValuePair<string, ActionData> action in actions)
+            {
+                if (action.Value.Description != null)
+                {
+                    Console.Error.WriteLine("  {0,-15} {1}", action.Key + ":", action.Value.Description);
+                }
+            }
+        }
+    }
+
+    public class ActionEventArgs : EventArgs
+    {
+        public string Action { get; private set; }
+
+        public ActionEventArgs(string action)
+        {
+            Action = action;
+        }
+    }
+}
