d53392b684c675acbe6a76b92ed522a1ce74510d
Michon van Dooren <michon1992@gmail.com>
2016-02-18 21:06:46 +0100

    Move ConfigDialog to the GUI namespace. Split up into multiple files and cleaned up

---
+++ CSharp-Utils/GUI/ConfigDialog.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Reflection;
+using System.Windows.Forms;
+using System.ComponentModel;
+using CSharpUtils.Utils;
+
+namespace CSharpUtils.GUI
+{
+    public partial class ConfigDialog<T> : Form
+    {
+        // ReSharper disable MemberCanBePrivate.Global
+        // ReSharper disable FieldCanBeMadeReadOnly.Global
+        /// <summary>
+        ///     Occurs when the config is being loaded.
+        /// </summary>
+        public EventHandler<EventArgs> Loading = (s, e) => { };
+
+        /// <summary>
+        ///     Occurs after the config has been loaded.
+        /// </summary>
+        public EventHandler<EventArgs> Loaded = (s, e) => { };
+
+        /// <summary>
+        ///     Occurs before the config is saved.
+        ///     This event can be cancelled to stop the save from happening.
+        /// </summary>
+        public EventHandler<CancelEventArgs> PreviewSaving = (s, e) => { };
+
+        /// <summary>
+        ///     Occurs when the config is being saved.
+        ///     This happens after all confirmations have happened, so at this point it is certain the save will happen.
+        /// </summary>
+        public EventHandler<EventArgs> Saving = (s, e) => { };
+
+        /// <summary>
+        ///     Occurs after the config has been saved.
+        /// </summary>
+        public EventHandler<EventArgs> Saved = (s, e) => { };
+        // ReSharper restore MemberCanBePrivate.Global
+        // ReSharper restore FieldCanBeMadeReadOnly.Global
+
+        private readonly T _settings;
+        private readonly Dictionary<string, Control> _settingsMap = new Dictionary<string, Control>();
+        private readonly TableLayoutPanel _panel;
+        private readonly Dictionary<string, ConfigDialogGroup> _groups = new Dictionary<string, ConfigDialogGroup>();
+
+        protected ConfigDialog(T settings)
+        {
+            this._settings = settings;
+
+            // Setup the form.
+            this.AutoSize = true;
+            this.MinimumSize = new Size(500, 500);
+            this.Text = "Settings";
+
+            // Create the main panel.
+            this._panel = new TableLayoutPanel
+            {
+                AutoSize = true,
+                ColumnCount = 1,
+                Dock = DockStyle.Fill,
+                RowCount = 0
+            };
+            this._panel.ColumnStyles.Add(new ColumnStyle());
+            this.Controls.Add(this._panel);
+
+            // Create the button panel.
+            Button buttonSave = new Button
+            {
+                Text = "Save",
+                DialogResult = DialogResult.OK
+            };
+            buttonSave.Click += (sender, e) =>
+            {
+                if (!this.SaveSettings())
+                {
+                    return;
+                }
+                this.Close();
+            };
+            Button buttonCancel = new Button
+            {
+                Text = "Cancel",
+                DialogResult = DialogResult.Cancel
+            };
+            buttonCancel.Click += (sender, e) =>
+            {
+                this.Close();
+            };
+            FlowLayoutPanel panelButton = new FlowLayoutPanel
+            {
+                AutoSize = true,
+                Dock = DockStyle.Bottom,
+                FlowDirection = FlowDirection.RightToLeft
+            };
+            panelButton.Controls.Add(buttonSave);
+            panelButton.Controls.Add(buttonCancel);
+            this.Controls.Add(panelButton);
+
+            // Some default event handlers.
+            this.Shown += this.ShownLoadSetttings;
+            this.PreviewSaving += this.PreviewSavingServices;
+            this.Saved += this.SavedServices;
+        }
+
+        /// <summary>
+        ///     The services that this settings screen is relevant for.
+        /// </summary>
+        protected ServicesCollection Services { private get; set; }
+
+        private void PreviewSavingServices(object sender, CancelEventArgs e)
+        {
+            if (this.Services == null)
+            {
+                return;
+            }
+
+            // Prompt to confirm saving/restarting.
+            DialogResult dialogResult = MessageBox.Show(
+                "The service(s) will now be restarted if currently running. Are you sure you want to proceed?",
+                "Save/restart service(s)",
+                MessageBoxButtons.YesNo
+            );
+            if (dialogResult != DialogResult.Yes)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void SavedServices(object sender, EventArgs e)
+        {
+            // Restart services.
+            this.Services?.InvokeRestart();
+        }
+
+        private void ShownLoadSetttings(object sender, EventArgs e)
+        {
+            // Automatically load the settings when the form is shown.
+            this.LoadSettings();
+        }
+
+        /// <summary>
+        ///     Add a header logo.
+        /// </summary>
+        /// <param name="logo">The logo</param>
+        protected void AddHeader(Image logo)
+        {
+            Label headerLabel = new Label
+            {
+                Dock = DockStyle.Top,
+                Image = logo,
+                ImageAlign = ContentAlignment.MiddleCenter,
+                MinimumSize = logo.Size + new Size(0, 20)
+            };
+            this.Controls.Add(headerLabel);
+        }
+
+        /// <summary>
+        ///     Create a new config group.
+        /// </summary>
+        /// <param name="name">The human-readable name/label of the group</param>
+        /// <returns>The new group</returns>
+        protected ConfigDialogGroup AddGroup(string name)
+        {
+            if (this._groups.ContainsKey(name))
+            {
+                throw new ArgumentException($"Group name already in use: {name}", nameof(name));
+            }
+
+            // Create the group
+            ConfigDialogGroup group = new ConfigDialogGroup(this);
+            group.Text = name;
+
+            // Store the group
+            this._panel.RowCount += 1;
+            this._panel.RowStyles.Add(new RowStyle());
+            this._panel.Controls.Add(group, 0, this._panel.RowCount - 1);
+            this._groups.Add(name, group);
+
+            return group;
+        }
+
+        /// <summary>
+        ///     Connect a propery to a control.
+        /// </summary>
+        /// <param name="propertyName">The property, as created in the ui/accessible through Properties.Settings.Default.X</param>
+        /// <param name="control">The control that this property should map to</param>
+        private void AddProperty(string propertyName, Control control)
+        {
+            PropertyInfo info = typeof (T).GetProperty(propertyName);
+            if (info == null)
+            {
+                throw new KeyNotFoundException($"Unknown property {propertyName}");
+            }
+            this._settingsMap.Add(propertyName, control);
+        }
+
+        /// <summary>
+        ///     Get the PropertyInfo for a given control.
+        ///     This can be used to get/set the value of the control to save/load it.
+        /// </summary>
+        /// <param name="control">The control to get the PropertyInfo for</param>
+        /// <returns>The PropertyInfo for the main data property for the type of the control</returns>
+        private static PropertyInfo GetPropertyForControl(Control control)
+        {
+            if (control is CheckBox)
+            {
+                return typeof (CheckBox).GetProperty("Checked");
+            }
+            if (control is NumericUpDown)
+            {
+                return typeof (NumericUpDown).GetProperty("Value");
+            }
+            return typeof (Control).GetProperty("Text");
+        }
+
+        /// <summary>
+        ///     Load the settings from the config into the controls.
+        /// </summary>
+        private void LoadSettings()
+        {
+            // Load the settings into the ui.
+            Type type = typeof (T);
+            foreach (KeyValuePair<string, Control> entry in this._settingsMap)
+            {
+                PropertyInfo settingsPropertyInfo = type.GetProperty(entry.Key);
+                PropertyInfo controlPropertyInfo = GetPropertyForControl(entry.Value);
+                controlPropertyInfo.SetValue(entry.Value,
+                    Convert.ChangeType(settingsPropertyInfo.GetValue(this._settings), controlPropertyInfo.PropertyType));
+            }
+
+            // Fire the events.
+            this.Loading(this, new EventArgs());
+            this.Loaded(this, new EventArgs());
+        }
+
+        /// <summary>
+        ///     Save the settings from the controls into the config.
+        /// </summary>
+        private bool SaveSettings()
+        {
+            // Check whether the save should continue.
+            CancelEventArgs e = new CancelEventArgs();
+            this.PreviewSaving(this, e);
+            if (e.Cancel)
+            {
+                return false;
+            }
+
+            // Set the properties.
+            Type type = typeof (T);
+            foreach (KeyValuePair<string, Control> entry in this._settingsMap)
+            {
+                PropertyInfo settingsPropertyInfo = type.GetProperty(entry.Key);
+                PropertyInfo controlPropertyInfo = GetPropertyForControl(entry.Value);
+                settingsPropertyInfo.SetValue(this._settings,
+                    Convert.ChangeType(controlPropertyInfo.GetValue(entry.Value), settingsPropertyInfo.PropertyType));
+            }
+            this.Saving(this, new EventArgs());
+
+            // Save the config.
+            type.GetMethod("Save").Invoke(this._settings, new object[0]);
+            this.Saved(this, new EventArgs());
+
+            return true;
+        }
+    }
+}
---
+++ CSharp-Utils/GUI/ConfigDialogGroup.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using CSharpUtils.Utils;
+
+namespace CSharpUtils.GUI
+{
+    partial class ConfigDialog<T>
+    {
+        protected class ConfigDialogGroup : GroupBox
+        {
+            private readonly ConfigDialog<T> _dialog;
+            private readonly TableLayoutPanel _panel;
+
+            public ConfigDialogGroup(ConfigDialog<T> dialog)
+            {
+                // Store the parent dialog.
+                this._dialog = dialog;
+
+                // Set the groupbox properties.
+                this.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+                this.AutoSize = true;
+
+                // Create the inner panel.
+                this._panel = new TableLayoutPanel
+                {
+                    AutoSize = true,
+                    ColumnCount = 2,
+                    Dock = DockStyle.Fill,
+                    RowCount = 0
+                };
+                this._panel.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 100F));
+                this._panel.ColumnStyles.Add(new ColumnStyle());
+                this.Controls.Add(this._panel);
+            }
+
+            #region Adding settings
+            // ReSharper disable UnusedMethodReturnValue.Global
+            /// <summary>
+            ///     Add a boolean property to this group.
+            /// </summary>
+            /// <param name="name">The property to control</param>
+            /// <param name="label">The label of the property</param>
+            /// <returns>The control for the property</returns>
+            public CheckBox AddBoolean(string name, string label)
+            {
+                // Create and register the control.
+                CheckBox control = new CheckBox();
+                this._dialog.AddProperty(name, control);
+
+                // Setup the control.
+                this.AddControl(label, control);
+
+                return control;
+            }
+
+            /// <summary>
+            ///     Add a numeric property to this group.
+            /// </summary>
+            /// <param name="name">The property to control</param>
+            /// <param name="label">The label of the property</param>
+            /// <returns>The control for the property</returns>
+            public NumericUpDown AddNumeric(string name, string label)
+            {
+                // Create and register the control.
+                NumericUpDown control = new NumericUpDown
+                {
+                    Minimum = decimal.MinValue,
+                    Maximum = decimal.MaxValue
+                };
+                this._dialog.AddProperty(name, control);
+
+                // Setup the control.
+                this.AddControl(label, control);
+
+                return control;
+            }
+
+            /// <summary>
+            ///     Add a textual property to this group.
+            /// </summary>
+            /// <param name="name">The property to control</param>
+            /// <param name="label">The label of the property</param>
+            /// <returns>The control for the property</returns>
+            public TextBox AddString(string name, string label)
+            {
+                // Create and register the control.
+                TextBox control = new TextBox();
+                this._dialog.AddProperty(name, control);
+
+                // Setup the control.
+                this.AddControl(label, control);
+
+                return control;
+            }
+
+            /// <summary>
+            ///     Add a textual property, with a button, to this group.
+            /// </summary>
+            /// <param name="name">The property to control</param>
+            /// <param name="label">The label of the property</param>
+            /// <param name="icon">The icon to add to the text box</param>
+            /// <returns>The control for the property</returns>
+            private Tuple<TextBox, Label> AddTextWithIcon(string name, string label, Icon icon)
+            {
+                // Create and register the main control.
+                TextBox control = new TextBox { Dock = DockStyle.Fill };
+                this._dialog.AddProperty(name, control);
+
+                // Create the button.
+                Label iconControl = new Label
+                {
+                    Image = icon.ToBitmap(),
+                    Dock = DockStyle.Right,
+                    MaximumSize = new Size(control.Height + 2, control.Height + 2)
+                };
+
+                // Add the icon to the text box, and make sure the text does not go below the icon.
+                control.Controls.Add(iconControl);
+                User32.SendMessage(control.Handle, 0xd3, (IntPtr)2, (IntPtr)(iconControl.Width << 16));
+
+                // Setup the control.
+                this.AddControl(label, control);
+
+                return new Tuple<TextBox, Label>(control, iconControl);
+            }
+
+            /// <summary>
+            ///     Add a folder path property to this group.
+            /// </summary>
+            /// <param name="name">The property to control</param>
+            /// <param name="label">The label of the property</param>
+            /// <returns>The controls for the property</returns>
+            public Tuple<TextBox, Label, FolderBrowserDialog> AddFolderPicker(string name, string label)
+            {
+                Tuple<TextBox, Label> controls = this.AddTextWithIcon(name, label, Resources.folder);
+
+                // Create the folder browser.
+                FolderBrowserDialog browser = new FolderBrowserDialog();
+
+                // Setup the button handler.
+                controls.Item2.Click += (sender, e) =>
+                {
+                    browser.SelectedPath = controls.Item1.Text;
+                    if (browser.ShowDialog() == DialogResult.OK)
+                    {
+                        controls.Item1.Text = browser.SelectedPath;
+                    }
+                };
+
+                return new Tuple<TextBox, Label, FolderBrowserDialog>(controls.Item1, controls.Item2, browser);
+            }
+
+            /// <summary>
+            ///     Add a file path property to this group.
+            /// </summary>
+            /// <param name="name">The property to control</param>
+            /// <param name="label">The label of the property</param>
+            /// <param name="create">Whether to use a picker to create new files</param>
+            /// <returns>The controls for the property</returns>
+            public Tuple<TextBox, Label, FileDialog> AddFilePicker(string name, string label, bool create)
+            {
+                Tuple<TextBox, Label> controls = this.AddTextWithIcon(name, label, create ? Resources.save : Resources.file);
+
+                // Create the file browser.
+                FileDialog browser = create ? (FileDialog)new SaveFileDialog() : new OpenFileDialog();
+
+                // Setup the button handler.
+                controls.Item2.Click += (sender, e) =>
+                {
+                    browser.FileName = controls.Item1.Text;
+                    if (browser.ShowDialog() == DialogResult.OK)
+                    {
+                        controls.Item1.Text = browser.FileName;
+                    }
+                };
+
+                return new Tuple<TextBox, Label, FileDialog>(controls.Item1, controls.Item2, browser);
+            }
+            // ReSharper enable UnusedMethodReturnValue.Global
+            #endregion
+
+            /// <summary>
+            ///     Add a control to the group.
+            /// </summary>
+            /// <param name="control">The control to add</param>
+            public void AddControl(Control control)
+            {
+                control.Dock = DockStyle.Fill;
+                this._panel.RowCount += 1;
+                this._panel.RowStyles.Add(new RowStyle());
+                this._panel.Controls.Add(control, 0, this._panel.RowCount - 1);
+                this._panel.SetColumnSpan(control, 2);
+            }
+
+            /// <summary>
+            ///     Add a property to this group.
+            /// </summary>
+            /// <param name="label">The label of the property</param>
+            /// <param name="control">The control for the property</param>
+            private void AddControl(string label, Control control)
+            {
+                // Setup the control's basic properties.
+                control.Dock = DockStyle.Fill;
+
+                // Create the label.
+                Label labelControl = new Label
+                {
+                    Text = label,
+                    Dock = DockStyle.Fill,
+                    TextAlign = ContentAlignment.MiddleLeft
+                };
+
+                // Add the elements to the groupbox.
+                this._panel.RowCount += 1;
+                this._panel.RowStyles.Add(new RowStyle());
+                this._panel.Controls.Add(labelControl, 0, this._panel.RowCount - 1);
+                this._panel.Controls.Add(control, 1, this._panel.RowCount - 1);
+            }
+        }
+    }
+}
